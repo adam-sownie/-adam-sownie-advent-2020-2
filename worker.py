517feac58b329a996053233973f9685767e38204
Adam Sownie <sownieadam@gmail.com>
2020-12-03 16:31:35 -0600

    added correct

--- worker.py
+++ worker.py
@@ -1,9 +1,25 @@
-f = open('input.txt', 'r')
-costs = f.readlines()
+import re
 
-for cost in costs:
-    for s_cost in costs:
-        for t_cost in costs:
-            if (int(cost) + int(s_cost) + int(t_cost)) == 2020:
-                print(int(cost) * int(s_cost) * int(t_cost))
-                exit()
+f = open('input.txt', 'r')
+passes = f.readlines()
+
+passed = 0
+for password in passes:
+    constraint = re.compile('\d+\-\d+').match(password)
+    min = int(constraint.group().split('-')[0])
+    max = int(constraint.group().split('-')[1])
+
+    password = password[(constraint.end() + 1):]
+    letter = password[0]
+    password = password[3:]
+
+    characterCount = 0
+
+    for character in password:
+        if character == letter:
+            characterCount += 1
+
+    if characterCount <= max and characterCount >= min:
+        passed += 1
+
+print(passed)
